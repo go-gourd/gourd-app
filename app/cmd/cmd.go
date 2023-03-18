24a6db0284b97b7414fb292a2f9924daf997d9b7
Kyour.cn <kyour@vip.qq.com>
2023-03-18 16:00:32 +0800

    [perf] 新增默认cmd执行命令

--- app/cmd/cmd.go
+++ app/cmd/cmd.go
@@ -7,6 +7,17 @@ import (
 
 func RegisterCmd() {
 
+	//默认命令行操作
+	cmd.SetDefault(cmd.Commend{
+		Handler: func(args []string) {
+			//这里让它默认启动
+			_ = cmd.Exec("start", []string{
+				args[0],
+				"start",
+			})
+		},
+	})
+
 	//命令行示例
 	cmd.Add(cmd.Commend{
 		Name:    "test",
